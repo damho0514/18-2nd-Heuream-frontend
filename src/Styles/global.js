c5408f814434b5f9f77ccfcb469488cf3d4f41e2
JAEWOOK JUNG <jaewookjung@JAEWOOKui-MacBookPro.local>
2021-03-29 21:42:39 +0900

    Add: 최종 초기세팅 완료

--- src/Styles/global.js
+++ src/Styles/global.js
@@ -1,1 +1,32 @@
-console.log("global");
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    *{
+        box-sizing:border-box;
+    }
+    body{
+        font-size:14px;
+        background-color:rgba(20,20,20,0.5);
+        font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+    }
+    a{
+        text-decoration:none;
+        color:inherit;
+        cursor: pointer;
+    }
+    ol, ul, li {
+        list-style: none;
+    }
+    input, button {
+        background-color: transparent;
+        border: none;
+        outline: none;
+    }
+    h1, h2, h3, h4, h5, h6 {
+    font-family:'Maven Pro', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
